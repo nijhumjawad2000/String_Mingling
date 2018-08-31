662b34f3e0f378eb25e148d5dfc683ed0093a7d1
nijhum_jawad2000 <njawad@techlaunch.io>
2018-08-30 20:23:08 -0400

    first commit

---
+++ string_mingling.js
@@ -0,0 +1,11 @@
+function stringMingling(str1,str2){
+    if(str1.length!==str2.length){
+        return;
+    }
+    if(str1.length===0){
+        return "";
+    }
+    return str1.charAt(0)+str2.charAt(0)+
+        stringMingling(str1.substring(1),str2.substring(1));
+}
+console.log(stringMingling("cat","dog"))
